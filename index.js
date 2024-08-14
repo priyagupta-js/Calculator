90454b58520f591a6a732e3df6d6a0b6ad1cb0d7
priyagupta-js <priyagupta052k@gmail.com>
2024-08-14 23:47:42 +0530

    initial files

---
+++ index.js
@@ -0,0 +1,14 @@
+
+let one = document.getElementById("one")
+let two = document.getElementById("two");
+let plus = document.getElementById("plus");
+let screen= document.getElementById("screen");
+
+function display(val){
+// screen.value = one.value ;
+// screen.value = two.value;
+screen.value +=val;
+}
+function addition(){
+
+}
