c58fe84f0c911d8dc726278ecb4ed308a8702ef1
priyagupta-js <priyagupta052k@gmail.com>
2024-08-19 00:27:40 +0530

    All clear button working

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ let seven = document.getElementById("seven");
 let eight = document.getElementById("eight");
 let nine = document.getElementById("nine");
 let zero = document.getElementById("zero");
-
+// let allClear = document.getElementById("allClear");
 // onclick function added to each element ; passed the number as argument 
 // append the value to the value of screen
 function display(val){
@@ -23,12 +23,13 @@ screen.value +=val;
 }
 
 // when clicked on the AC button , all the contents should be removed
-function clear(){
-
+function allClear()
+{
+screen.value = 0;
 }
-function Del(){
+// function Del(){
 
-}
-function addition(){
+// }
+// function addition(){
 
-}
+// }
