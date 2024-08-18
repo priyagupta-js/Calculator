ece20c599fb203df164e9a744e8ee36a310fc39e
priyagupta-js <priyagupta052k@gmail.com>
2024-08-18 23:58:05 +0530

    worked on All clear button

--- index.js
+++ index.js
@@ -4,10 +4,27 @@ let two = document.getElementById("two");
 let plus = document.getElementById("plus");
 let screen= document.getElementById("screen");
 let three = document.getElementById("three");
+let four = document.getElementById("four");
+let five = document.getElementById("five");
+let six = document.getElementById("six");
+let seven = document.getElementById("seven");
+let eight = document.getElementById("eight");
+let nine = document.getElementById("nine");
+let zero = document.getElementById("zero");
+
+// onclick function added to each element ; passed the number as argument 
+// append the value to the value of screen
 function display(val){
+
 // screen.value = one.value ;
 // screen.value = two.value;
+
 screen.value +=val;
+}
+
+// when clicked on the AC button , all the contents should be removed
+function clear(){
+
 }
 function Del(){
 
