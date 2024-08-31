bbd81aee99f7f2f019785dda71a9553266dcc341
priyagupta-js <priyagupta052k@gmail.com>
2024-08-31 23:51:46 +0530

    Delete  button working

--- index.js
+++ index.js
@@ -11,9 +11,10 @@ let seven = document.getElementById("seven");
 let eight = document.getElementById("eight");
 let nine = document.getElementById("nine");
 let zero = document.getElementById("zero");
-// let allClear = document.getElementById("allClear");
-// onclick function added to each element ; passed the number as argument 
-// append the value to the value of screen
+
+
+
+// one.addEventListener("click" , addition());
 
 function display(val){
 
@@ -28,10 +29,12 @@ function allClear()
 {
 screen.value = "";
 }
+
+// delete the last element 
 function Del(){
     screen.value = screen.value.slice(0,-1);
     
 }
 function addition(){
-
+console.log("Bye!")
 }
