3bec32bb8a1c8006c1960904b584b17e2fb17cae
priyagupta-js <priyagupta052k@gmail.com>
2024-09-03 23:58:44 +0530

    Optimize the code

--- index.js
+++ index.js
@@ -1,18 +1,4 @@
 
-// let one = document.getElementById("one")
-// let two = document.getElementById("two");
-// let plus = document.getElementById("plus");
-// let screen= document.getElementById("screen");
-// let three = document.getElementById("three");
-// let four = document.getElementById("four");
-// let five = document.getElementById("five");
-// let six = document.getElementById("six");
-// let seven = document.getElementById("seven");
-// let eight = document.getElementById("eight");
-// let nine = document.getElementById("nine");
-// let zero = document.getElementById("zero");
-
-
 let buttons = document.querySelectorAll("input");
 
 // one.addEventListener("click" , addition());
