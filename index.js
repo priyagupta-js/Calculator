7d5b1ca5cf89148b7b63c2b42b7be077b64dd4c6
priyagupta-js <priyagupta052k@gmail.com>
2024-09-05 23:58:01 +0530

    using switch case

--- index.js
+++ index.js
@@ -2,27 +2,31 @@ let string= "";
 let display = document.querySelector(".screen");
 let buttons = document.querySelectorAll(".button");
 
-Array.from(buttons).forEach((button)=>{
-button.addEventListener('click' , (e)=>{
-    if (e.target.innerHTML == "=")
-    {
-        string = eval(string);
-        display.value = string;
-    }
-    else if (e.target.innerHTML == "Del")
-    {
-        string = display.value.slice(0,-1);
-        display.value =string;
-    }
-    else if (e.target.innerHTML == "AC")
-        {
-            display.value ="";
-        }
-    else
-    {
-        string = string + e.target.innerHTML;
-        display.value = string;
-    }
+// symbols
+let plus = document.getElementById("btn");
+let minus = document.getElementById("btn");
 
-});
-});
+// Array.from(buttons).forEach((button)=>{
+// button.addEventListener('click' , (e)=>{
+//     if (e.target.innerHTML == "=")
+//     {
+//         string = eval(string);
+//         display.value = string;
+//     }
+//     else if (e.target.innerHTML == "Del")
+//     {
+//         string = display.value.slice(0,-1);
+//         display.value =string;
+//     }
+//     else if (e.target.innerHTML == "AC")
+//         {
+//             display.value ="";
+//         }
+//     else
+//     {
+//         string = string + e.target.innerHTML;
+//         display.value = string;
+//     }
+
+// });
+// });
