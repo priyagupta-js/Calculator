6c1986e3341a455483d7254cd48f6f42ce191c1e
priyagupta-js <priyagupta052k@gmail.com>
2024-09-09 23:01:21 +0530

    all files

--- index.js
+++ index.js
@@ -1,36 +1,36 @@
-let string= "";
-let display = document.querySelector(".screen");
-let buttons = document.querySelectorAll(".button");
+// let string= "";
+// let display = document.querySelector(".screen");
+// let buttons = document.querySelectorAll(".button");
 
-// symbols
-let plus = document.getElementById("btn");
-let minus = document.getElementById("btn");
-let multi = document.getElementById("btn");
-let divide = document.getElementById("btn");
-let mod =  document.getElementById("btn");
+// // symbols
+// let plus = document.getElementById("btn");
+// let minus = document.getElementById("btn");
+// let multi = document.getElementById("btn");
+// let divide = document.getElementById("btn");
+// let mod =  document.getElementById("btn");
 
 
-// Array.from(buttons).forEach((button)=>{
-// button.addEventListener('click' , (e)=>{
-//     if (e.target.innerHTML == "=")
-//     {
-//         string = eval(string);
-//         display.value = string;
-//     }
-//     else if (e.target.innerHTML == "Del")
-//     {
-//         string = display.value.slice(0,-1);
-//         display.value =string;
-//     }
-//     else if (e.target.innerHTML == "AC")
-//         {
-//             display.value ="";
-//         }
-//     else
-//     {
-//         string = string + e.target.innerHTML;
-//         display.value = string;
-//     }
+Array.from(buttons).forEach((button)=>{
+button.addEventListener('click' , (e)=>{
+    if (e.target.innerHTML == "=")
+    {
+        string = eval(string);
+        display.value = string;
+    }
+    else if (e.target.innerHTML == "Del")
+    {
+        string = display.value.slice(0,-1);
+        display.value =string;
+    }
+    else if (e.target.innerHTML == "AC")
+        {
+            display.value ="";
+        }
+    else
+    {
+        string = string + e.target.innerHTML;
+        display.value = string;
+    }
 
-// });
-// });
+});
+});
